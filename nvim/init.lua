fea9d56e552544218c3901eae5d4344fb306df09
Matteo Bertolino <m.bertolino.m@gmail.com>
2025-07-07 16:29:10 +0200

    nvim: Change color for invisible characters
    Signed-off-by: Matteo Bertolino <m.bertolino.m@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,22 @@
--- init.lua: Personal NeoVim initial configuration file
+-- init.lua: Personal Neovim initial configuration file
 -- Matteo Bertolino <m.bertolino.m@gmail.com>
--- Mon Jul 07 2025 16:07:43 CEST
+-- Mon Jul 07 2025 16:28:27 CEST
 
 -- This is free and unencumbered software released into the public domain.
 
 -- Fucking thing to make the stupid wayland clipboard work
 -- I wholeheartedly despise clipboard provider
 vim.g.clipboard = {
-    name = "wl-clipboard",
-    copy = {
-        ["+"] = "wl-copy --foreground --type text/plain",
-        ["*"] = "wl-copy --foreground --type text/plain",
-    },
-    paste = {
-        ["+"] = "wl-paste --no-newline",
-        ["*"] = "wl-paste --no-newline",
-    },
-    cache_enabled = true,
+	name = "wl-clipboard",
+	copy = {
+		["+"] = "wl-copy --foreground --type text/plain",
+		["*"] = "wl-copy --foreground --type text/plain",
+	},
+	paste = {
+		["+"] = "wl-paste --no-newline",
+		["*"] = "wl-paste --no-newline",
+	},
+	cache_enabled = true,
 }
 
 vim.cmd "colorscheme vim"
@@ -26,7 +26,9 @@ vim.cmd "map <Left> <Nop>"
 vim.cmd "map <Right> <Nop>"
 vim.cmd "map <Down> <Nop>"
 
-vim.opt.relativenumber = true
+-- At least some line number is on
+vim.opt.number = true
+vim.opt.relativenumber =true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -44,3 +46,14 @@ vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 300
 vim.opt.timeout = true
 vim.opt.timeoutlen = 500
+
+-- Invisible characters
+vim.opt.listchars = {
+	tab = '→ ',
+	space = '·',
+	trail = '•',
+	eol = '↴'
+}
+
+-- Colors for invisible characters
+vim.api.nvim_set_hl(0, 'NonText', { fg = '#323232' })         -- Caratteri di fine riga
